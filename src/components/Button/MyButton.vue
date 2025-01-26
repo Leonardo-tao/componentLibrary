45b79bf61ad177857edc85b729825d5390e6533c
Tao <codertt811@163.com>
2025-01-26 21:22:29 +0800

    add button

--- src/components/Button/MyButton.vue
+++ src/components/Button/MyButton.vue
@@ -1,10 +1,28 @@
 <template>
   <div>
+    <button
+      class="t-btn"
+      :class="{
+        [`t-btn--${type}`]: type,
+        [`t-btn--${size}`]: size,
+        'is-plain': plain,
+        'is-round': round,
+        'is-circle': circle,
+        'is-disabled': disabled,
+        'is-loading': loading
+      }"
+      :disabled="disabled"
+    ></button>
 
+    <span>
+      <slot></slot>
+    </span>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { ButtonProps } from './types.ts';
+defineProps<ButtonProps>()
 
 </script>
 
