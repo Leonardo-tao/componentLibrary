eac71372ef08c962fe7652f62575f7beea267e97
Tao <codertt811@163.com>
2025-02-11 22:18:58 +0800

    import 'fas' icon library

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import './styles/index.css'
 
 import { createApp } from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import App from './App.vue'
 import router from './router'
 
+library.add(fas)
 const app = createApp(App)
 
-app.use(router)
-
-app.mount('#app')
+app
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .use(router)
+  .mount('#app')
